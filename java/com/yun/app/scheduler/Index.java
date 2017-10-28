6bc4279c31301e24b5cd67d1c1b9dca880b05ae6
yimyj1210 <yimyj1210@gmail.com>
2017-10-28 18:26:28 +0900

    오후 6:13 2017-10-28

---
+++ java/com/yun/app/scheduler/Index.java
@@ -0,0 +1,13 @@
+package com.yun.app.scheduler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Index extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index);
+    }
+}
